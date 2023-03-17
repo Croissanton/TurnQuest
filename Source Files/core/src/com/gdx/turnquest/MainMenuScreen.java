439e5c29fe4c2e22502920ede331c2ad25683d49
Cristian Ruiz Martín <cristianruizm@uma.es>
2023-03-17 22:27:01 +0100

    Fixed background and added clickable button

--- Source Files/core/src/com/gdx/turnquest/MainMenuScreen.java
+++ Source Files/core/src/com/gdx/turnquest/MainMenuScreen.java
@@ -6,10 +6,13 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -29,6 +32,8 @@ public class MainMenuScreen implements Screen {
 
     public static final int VIRTUAL_HEIGHT = dm.height;
 
+    public Stage stage;
+
     Viewport viewport;
 
 
@@ -44,33 +49,30 @@ public class MainMenuScreen implements Screen {
 
         viewport = new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, camera);
 
-        Stage stage = new Stage(new ScreenViewport());
+        stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
 
         //ATTEMPT OF CREATING A MAIN MENU WITH A TABLE W/ BUTTONS (failed)
         //does not show but it causes no errors, suspected bad position in code
 
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-        stage.draw();
-
-        Table table = new Table();
-        table.setFillParent(true);
-        table.setDebug(true);
-        stage.addActor(table);
 
         Skin skin = new Skin(Gdx.files.internal("glassy-ui.json"));
 
-        TextButton newGame = new TextButton("New Game", skin);
-        TextButton preferences = new TextButton("Preferences", skin);
-        TextButton exit = new TextButton("Exit", skin);
+        TextButton button = new TextButton("Click me!", skin);
+        button.setSize(1000, 500); // set the size of the button to 100 pixels wide and 50 pixels tall
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Handle button click here
+            }
+        });
 
+        Table table = new Table();
+        table.setFillParent(true);
+        table.add(button).center().padBottom(50f);
 
-        table.add(newGame).fillX().uniformX();
-        table.row().pad(10, 0, 10, 0);
-        table.add(preferences).fillX().uniformX();
-        table.row();
-        table.add(exit).fillX().uniformX();
+        stage.addActor(table);
 
         viewport = new ScreenViewport();
 
@@ -95,7 +97,7 @@ public class MainMenuScreen implements Screen {
         game.batch.begin();
 
 
-        game.batch.draw(backgroundTexture,0,0);
+        game.batch.draw(backgroundTexture, 0, 0, VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
         game.font.getData().setScale(2); //Changes font size.
         game.font.draw(game.batch, "Welcome to TurnQuest! ", 100, 400);
         game.font.draw(game.batch, "Click anywhere to begin! ", 100, 350);
@@ -108,10 +110,8 @@ public class MainMenuScreen implements Screen {
         }
         game.batch.end();
 
-//        if (Gdx.input.isTouched()) {
-//            game.setScreen(new GameScreen(game));
-//            dispose();
-//        }
+        stage.act();
+        stage.draw();
 
     }
 
