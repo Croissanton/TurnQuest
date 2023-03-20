96a16a78fd78c1ab95659851f4b90a68f0f0a3e4
mikzb <mikolajzabski@uma.es>
2023-03-20 10:12:25 +0100

    PrefDialog++

--- Source Files/core/src/com/gdx/turnquest/PreferencesDialog.java
+++ Source Files/core/src/com/gdx/turnquest/PreferencesDialog.java
@@ -1,18 +1,28 @@
 package com.gdx.turnquest;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import static com.gdx.turnquest.MainMenuScreen.*;
-
 public class PreferencesDialog extends Dialog {
+
+
+    public static final int VIRTUAL_WIDTH = dm.width;
+
+    public static final int VIRTUAL_HEIGHT = dm.height;
+
+
+
 
     public int MAINVOLUME = 30;
     private Runnable yesRunnable;
+
 
     public PreferencesDialog(String title, String message, Runnable yesRunnable, Skin skin) {
         super(title, skin);
@@ -21,6 +31,14 @@ public class PreferencesDialog extends Dialog {
         final CheckBox fullscreen = new CheckBox("Fullscreen",skin);
         final Slider mainVolume = new Slider(0,100,1,false,skin);
         TextButton bBack = new TextButton("Back", skin);
+
+
+        getContentTable().defaults().pad(10);
+        getContentTable().add(fullscreen).width(200);
+        getContentTable().row();
+        getContentTable().add(mainVolume).width(200);
+        getContentTable().add(bBack).width(200);
+
 
         fullscreen.addListener(new ChangeListener() {
             @Override
