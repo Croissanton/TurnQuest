a460e8c6a75a7734072c2a303e6042eb3b4cb306
mikzb <mikolajzabski@uma.es>
2023-04-16 21:27:36 +0200

    Implemented basic ShopScreen

--- Source Files/core/src/com/gdx/turnquest/ShopScreen.java
+++ Source Files/core/src/com/gdx/turnquest/ShopScreen.java
@@ -3,42 +3,112 @@ package com.gdx.turnquest;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import javax.swing.plaf.synth.Region;
+
 import static com.gdx.turnquest.TurnQuest.*;
+import static com.gdx.turnquest.TurnQuest.getVirtualHeight;
+import static java.lang.Math.min;
 
 public class ShopScreen implements Screen {
     final TurnQuest game;
 
+    private Stage stage;
+    private Table itemTable;
+    private ScrollPane scrollPane;
+
+    private int CellWidth=100;
+    private int CellHeight=50;
+
+
+
     public ShopScreen(final TurnQuest game) {
         this.game = game;
 
-        setStage(new Stage(getViewport()));
-        Gdx.input.setInputProcessor(getStage());
+        stage = new Stage(TurnQuest.getViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        setBackgroundTexture(new Texture(Gdx.files.internal("Pixel art forest/Preview/Background.png")));
 
 
-        TextButton bReturn = new TextButton("Return", getSkin());
+        // Create the table to hold the items
+        itemTable = new Table(getSkin());
+        itemTable.defaults().pad(30).width(CellWidth).height(CellHeight);
+        itemTable.columnDefaults(0).align(Align.center).width(CellWidth);
+        itemTable.columnDefaults(1).width(CellWidth);
+        itemTable.columnDefaults(2).padLeft(50).padRight(CellWidth/2).width(CellWidth);
+        itemTable.columnDefaults(3).padLeft(50).padRight(CellWidth/2).width(CellWidth);
 
-        bReturn.addListener(new ClickListener() {
+        //DEBUG
+        itemTable.add("priceLabel");
+        itemTable.add("itemImage");
+        itemTable.add("buyButton");
+        itemTable.add("sellButton");
+        itemTable.row();
+
+        // Add items to the table
+        for (int i = 1; i <= 24; i++) {
+            // Create label for item price
+            Label priceLabel = new Label(String.valueOf(i),getSkin());
+
+            // Load item texture
+            Texture itemTexture = new Texture(Gdx.files.internal("items/Icons_no_background/rings_and_necklaces/rpg_icons" + i + ".png"));
+            double aspectRatio = itemTexture.getWidth()/itemTexture.getHeight();
+            int newWidth = (int) (aspectRatio*itemTexture.getWidth());
+            TextureRegion itemRegion = new TextureRegion(itemTexture,newWidth,CellHeight);
+            Image itemImage = new Image(itemTexture);
+
+
+            // Create buy/sell buttons for item
+            TextButton buyButton = new TextButton("Buy", getSkin());
+            TextButton sellButton = new TextButton("Sell", getSkin());
+
+            // Add item components to the item table
+            itemTable.add(priceLabel);
+            itemTable.add(itemImage);
+            itemTable.add(buyButton);
+            itemTable.add(sellButton);
+            itemTable.row();
+        }
+
+        // Create a scroll pane to hold the item table
+        scrollPane = new ScrollPane(itemTable, getSkin());
+        scrollPane.setScrollingDisabled(true, false);
+        scrollPane.setSmoothScrolling(true);
+
+        // Add the scroll pane to the root table of the stage
+        Table rootTable = new Table(getSkin());
+        rootTable.setFillParent(true);
+        rootTable.add(scrollPane).expand().fill();
+        stage.addActor(rootTable);
+
+        // Create back button
+        TextButton backButton = new TextButton("Back", getSkin());
+        backButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 game.setScreen(new GameScreen(game));
             }
         });
-
-        Table table = new Table();
-        table.setFillParent(true);
-        table.add(bReturn).center().padBottom(50f).row();
-
-        table.padTop(100f); // add some padding at the top
-
-        getStage().addActor(table);
-
-        getViewport().apply();
+        // Add back button to a separate table
+        Table backButtonTable = new Table(getSkin());
+        backButtonTable.add(backButton).pad(40);
+        // Add back button table to the root table
+        rootTable.add(backButtonTable).top().left().expandX();
     }
 
     @Override
@@ -48,28 +118,23 @@ public class ShopScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0.3f, 0.7f, 0.8f, 1); // You can also write a color here, this is the background.
-
-        getCamera().update();
-        getBatch().setProjectionMatrix(getCamera().combined);
+        ScreenUtils.clear(0.3f, 0.7f, 0.8f, 1);
 
         getBatch().begin();
         getBatch().draw(getBackgroundTexture(), 0, 0, getVirtualWidth(), getVirtualHeight());
-        getFont().getData().setScale(4); //Changes font size.
-        getFont().draw(getBatch(), "Shop", getVirtualWidth()*45/100, getVirtualHeight()*85/100);
         getBatch().end();
 
-        getStage().act();
-        getStage().draw();
+        stage.act();
+        stage.draw();
 
         if(Gdx.input.isKeyJustPressed(Input.Keys.F11)) {
-            toggleFullscreen();
+            TurnQuest.toggleFullscreen();
         }
     }
 
     @Override
     public void resize(int width, int height) {
-        getViewport().update(width, height, true);
+        TurnQuest.getViewport().update(width, height, true);
     }
 
     @Override
@@ -89,7 +154,6 @@ public class ShopScreen implements Screen {
 
     @Override
     public void dispose() {
-        getStage().dispose();
-        getBackgroundTexture().dispose();
+        stage.dispose();
     }
 }
