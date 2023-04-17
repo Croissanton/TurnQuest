a48c9adff395470261fb093e8395b7f874e0cddc
Pablo <pabloruar03@uma.es>
2023-04-17 09:23:03 +0200

    shop fixed

--- Source Files/core/src/com/gdx/turnquest/ShopScreen.java
+++ Source Files/core/src/com/gdx/turnquest/ShopScreen.java
@@ -3,32 +3,18 @@ package com.gdx.turnquest;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.ScreenUtils;
-
-import javax.swing.plaf.synth.Region;
-
 import static com.gdx.turnquest.TurnQuest.*;
-import static com.gdx.turnquest.TurnQuest.getVirtualHeight;
-import static java.lang.Math.min;
 
 public class ShopScreen implements Screen {
     final TurnQuest game;
-
-    private Stage stage;
-    private Table itemTable;
     private ScrollPane scrollPane;
 
     private int CellWidth=100;
@@ -42,14 +28,14 @@ public class ShopScreen implements Screen {
         this.game = game;
         this.player = player;
 
-        stage = new Stage(TurnQuest.getViewport());
-        Gdx.input.setInputProcessor(stage);
+        setStage(new Stage(getViewport()));
+        Gdx.input.setInputProcessor(getStage());
 
         setBackgroundTexture(new Texture(Gdx.files.internal("Pixel art forest/Preview/Background.png")));
 
 
         // Create the table to hold the items
-        itemTable = new Table(getSkin());
+        Table itemTable = new Table(getSkin());
         itemTable.defaults().pad(30).width(CellWidth).height(CellHeight);
         itemTable.columnDefaults(0).align(Align.center).width(CellWidth);
         itemTable.columnDefaults(1).width(CellWidth);
@@ -97,7 +83,7 @@ public class ShopScreen implements Screen {
         Table rootTable = new Table(getSkin());
         rootTable.setFillParent(true);
         rootTable.add(scrollPane).expand().fill();
-        stage.addActor(rootTable);
+        getStage().addActor(rootTable);
 
         // Create back button
         TextButton backButton = new TextButton("Back", getSkin());
@@ -127,8 +113,8 @@ public class ShopScreen implements Screen {
         getBatch().draw(getBackgroundTexture(), 0, 0, getVirtualWidth(), getVirtualHeight());
         getBatch().end();
 
-        stage.act();
-        stage.draw();
+        getStage().act();
+        getStage().draw();
 
         if(Gdx.input.isKeyJustPressed(Input.Keys.F11)) {
             TurnQuest.toggleFullscreen();
@@ -157,6 +143,6 @@ public class ShopScreen implements Screen {
 
     @Override
     public void dispose() {
-        stage.dispose();
+        getStage().dispose();
     }
 }
