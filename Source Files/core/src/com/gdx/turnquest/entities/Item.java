51f4012e772a324616d48d1a8ab8a375d29aba3d
Cristian Ruiz Martín <cristianruizm@uma.es>
2023-04-29 15:33:59 +0200

    some more changes

--- Source Files/core/src/com/gdx/turnquest/entities/Item.java
+++ Source Files/core/src/com/gdx/turnquest/entities/Item.java
@@ -8,16 +8,20 @@ public class Item {
     private final int value;
     private ObjectMap<String, Integer> stats;
     private final String type;
-
     private final String classType;
+    private final String imagePath;
 
-    public Item(int id, String name, String description, int value, ObjectMap<String, Integer> stats, String type, String classType) {
-        if(value < 0 || type == "CONSUMABLE" || type == "EQUIPMENT") throw new IllegalArgumentException("Incorrect value or type");
+    public Item(String name, String description, int value, ObjectMap<String, Integer> stats, String type, String classType, String imagePath) {
+        if(value < 0) throw new IllegalArgumentException("Invalid value.");
+        if(stats.isEmpty() || stats.size > 7) throw new IllegalArgumentException("Invalid stats.");
+        if(!type.equals("CONSUMABLE") && !type.equals("WEAPON_1") && !type.equals("WEAPON_2") && !type.equals("ARMOR") && !type.equals("RING")) throw new IllegalArgumentException("Invalid type.");
+        if(!classType.equals("Warrior") && !classType.equals("Archer") && !classType.equals("Mage") && !classType.equals("All")) throw new IllegalArgumentException("Invalid class type.");
         this.name = name;
         this.description = description;
         this.value = value;
         this.stats = stats;
         this.type = type;
+        this.imagePath = imagePath;
         this.classType = classType; //Can be "Warrior", "Mage", "Archer", "All"
     }
 
