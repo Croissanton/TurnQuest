4a54f2ca8046a79dd934a4e67f19740dd51d2c8c
michael <ge93cik@mytum.de>
2023-05-03 19:39:33 +0200

    implemented update Timer && getTimeDiff for resetting counters

--- Source Files/core/src/com/gdx/turnquest/dialogs/LoginDialog.java
+++ Source Files/core/src/com/gdx/turnquest/dialogs/LoginDialog.java
@@ -18,6 +18,12 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
 
 
 import static com.gdx.turnquest.TurnQuest.hasInternetConnection;
@@ -69,15 +75,18 @@ public class LoginDialog extends Dialog {
                 if (!userManager.checkUser(username, password)) {
                     // If the credentials are not valid, display an error message
                     errorLabel.setText("Invalid username or password.");
-              } else if (!checkLoginCount(username)) {
-                    errorLabel.setText("No logins left");/*TODO: Dialog goes back to main screen without user interaction, fix this */
                 } else {
-                    // If the credentials are valid, proceed with the login process
-                    PlayerManager playerManager = new PlayerManager();
-                    Player player = playerManager.getPlayer(username);
-                    game.setCurrentPlayer(player);
-                    hide();
-                    game.setScreen(new GameScreen(game));
+                    updateTimer(username, "login");
+                    if (!checkLoginCount(username)) {
+                        errorLabel.setText("No logins left");/*TODO: Dialog goes back to main screen without user interaction, fix this */
+                    } else {
+                        // If the credentials are valid, proceed with the login process
+                        PlayerManager playerManager = new PlayerManager();
+                        Player player = playerManager.getPlayer(username);
+                        game.setCurrentPlayer(player);
+                        hide();
+                        game.setScreen(new GameScreen(game));
+                    }
                 }
             }
         }
@@ -114,7 +123,7 @@ public class LoginDialog extends Dialog {
             JSONObject loginInfo = (JSONObject) loginData.get(username);
             long loginCount = (long) loginInfo.get("loginCount");
 
-            if (loginCount < 5000) {   /*The Counter should be set to 5 for the actual game */
+            if (loginCount < 5000) {   /*TODO: The Counter should be set to 5 for the actual game, this is just for testing */
                 loginCount++;
                 loginInfo.put("loginCount", loginCount);
 
@@ -131,6 +140,66 @@ public class LoginDialog extends Dialog {
         } catch (IOException | ParseException ex) {
             ex.printStackTrace();
             return false;
+        }
+    }
+
+
+
+
+    private float getTimeDiff(String username,String timetype) {
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject Users = (JSONObject) parser.parse(new FileReader("../Data/users.json"));
+
+            JSONObject user = (JSONObject) Users.get(username);
+            //Get the saved first login time as a Calender Object
+            String fTime = (String) user.get(timetype+"Time");
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            try {
+                Date firstTime = formatter.parse(fTime);
+                Calendar FirstTime = Calendar.getInstance();
+                // set the calendar object to the given time
+                FirstTime.setTime(firstTime);
+                long diff = System.currentTimeMillis()-FirstTime.getTimeInMillis();
+                //Time differnce as a float in hours
+
+                return diff / (60.0f * 60.0f * 1000.0f);
+
+            } catch (java.text.ParseException e) {
+                return 0;
+            }
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+    public void updateTimer(String username,String timetype) {
+
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject Users = (JSONObject) parser.parse(new FileReader("../Data/users.json"));
+
+            JSONObject user = (JSONObject) Users.get(username);
+            //Get the saved first login time as a Calender Object
+
+            if (getTimeDiff(username,timetype)>24){
+                LocalDateTime now = LocalDateTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+                String formattedDateTime = now.format(formatter);
+
+                user.put(timetype+"Time", formattedDateTime);
+                user.put(timetype+"Count", 0);
+
+                // Write the updated JSONObject back to the JSON file
+                FileWriter fileWriter = new FileWriter("../Data/users.json");
+                fileWriter.write(Users.toJSONString());
+                fileWriter.flush();
+                fileWriter.close();
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
         }
     }
 }
